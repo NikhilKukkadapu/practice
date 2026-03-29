c5e24d6da915991d32ac11a35a3e683b3caa0baa
Nikhil <nikhilkukkadapu1@gmail.com>
2026-03-29 22:39:28 +0530

    practice

---
+++ class Codechef {.java
@@ -0,0 +1,33 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare two floating-point variables to store scores
+        double score1 = 85.5;
+        double score2 = 90.0;
+        
+        // Perform floating-point division to calculate the average
+        double average = (score1 + score2) / 2;
+        
+        // Print the calculated average
+        System.out.println("Average Score: " + average);
+    }
+}
+
+
+class Codechef {
+    public static void main(String[] args) {
+        // Declare integer variables
+        int num1 = 20;
+        int num2 = 6;
+        int num3 = 15;
+        int num4 = 4;
+
+        // Perform modulus operation
+        int remainder1 = num1 % num2; // 20 % 6 = 2
+        int remainder2 = num3 % num4; // 15 % 4 = 3
+
+        // Print results
+        System.out.println("Remainder when 20 is divided by 6 : " + remainder1);
+        System.out.println("Remainder when 15 is divided by 4 : " + remainder2);
+    }
+}
+
