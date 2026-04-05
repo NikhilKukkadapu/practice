22a52c27e6892fc63578a2cff1afb7f2403ebfd5
Nikhil <nikhilkukkadapu1@gmail.com>
2026-04-05 22:17:41 +0530

    Practice

---
+++ 1.java
@@ -0,0 +1,16 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare and assign multiple int variables in one statement
+        int goalsScored = 3, assistsMade = 2, shotsOnTarget = 5;
+
+        // Declare and assign multiple double variables in one statement
+        double playerRating1 = 8.5, playerRating2 = 7.9;
+
+        // Print all values
+        System.out.println("Goals Scored: " + goalsScored);
+        System.out.println("Assists Made: " + assistsMade);
+        System.out.println("Shots on Target: " + shotsOnTarget);
+        System.out.println("Player 1 Rating: " + playerRating1);
+        System.out.println("Player 2 Rating: " + playerRating2);
+    }
+}
